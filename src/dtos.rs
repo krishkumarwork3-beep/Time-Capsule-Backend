c25973a53aa02d6295190556c5a69e7debcde4b2
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-23 07:17:05 +0530

    add capsule domain model

--- src/dtos.rs
+++ src/dtos.rs
@@ -3,3 +3,17 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use validator::Validate;
+
+#[derive(Debug, Clone)]
+pub struct Capsule {
+    pub id: Uuid,
+    pub public_id: String,
+    pub name: String,
+    pub email: String,
+    pub title: String,
+    pub message: String,
+    pub unlock_at: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub is_unlocked: Option<bool>,
+    pub email_sent: Option<bool>,
+}
