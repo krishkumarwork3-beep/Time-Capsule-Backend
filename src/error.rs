bfb13b2cf38abd5caf96d555fccddb5dc0081e6c
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-23 07:32:43 +0530

    add generic HTTP error constructor

--- src/error.rs
+++ src/error.rs
@@ -32,3 +32,12 @@ pub struct HttpError {
 }
 
 impl HttpError {
+    pub fn new(
+        message: impl Into<String>,
+        status: StatusCode
+    ) -> Self {
+        HttpError {
+            message: message.into(),
+            status,
+        }
+    }
